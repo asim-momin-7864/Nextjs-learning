8d3e866922fa135a35b9dc0a5a9b2065a24ede6b
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-08-25 21:27:36 +0530

    feat: Implement auth session mechanisum logic

---
+++ nextjs-basic-app/lib/auth.ts
@@ -0,0 +1,56 @@
+import { SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+
+const secretKey = process.env.SESSION_SECRET;
+if (!secretKey) {
+  throw new Error("SESSION_SECRET is missing");
+}
+const encodedKey = new TextEncoder().encode(secretKey);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function encrypt(payload: any) {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("7d")
+    .sign(encodedKey);
+}
+
+export async function decrypt(session: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (error) {
+    console.error("Error decrypting session:", error);
+    return null; // Return null if verification fails
+  }
+}
+
+export async function createSession(userId: string) {
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 days
+  const session = await encrypt({ userId, expiresAt });
+
+  // Need to await cookies() in Next.js 16
+  const cookieStore = await cookies();
+  cookieStore.set("session", session, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
+}
+
+export async function deleteSession() {
+  const cookieStore = await cookies();
+  cookieStore.delete("session");
+}
+
+export async function getSession() {
+  const cookieStore = await cookies();
+  const session = cookieStore.get("session")?.value;
+  if (!session) return null;
+  return await decrypt(session);
+}
