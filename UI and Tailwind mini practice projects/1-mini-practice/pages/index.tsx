db6632db1924172716e2e0f05c73b1aaf0a56ba1
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-08-31 20:42:38 +0530

    feat: initialize Next.js project with Tailwind CSS setup

---
+++ UI and Tailwind mini practice projects/1-mini-practice/pages/index.tsx
@@ -0,0 +1,25 @@
+export default function TailwindPractice() {
+  return (
+    <div className="bg-slate-900 ">
+      <header>hiii</header>
+      <section className="space-y-5 text-center max-w-2xl mx-auto px-5 py-12">
+        <h1 className="text-4xl font-extrabold">
+          {" "}
+          Tailwind Hyper Class Website{" "}
+        </h1>
+        <p className="text-[1.2rem]">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+          pariatur, delectus sequi voluptatem eos natus mollitia architecto ex
+          eius dicta quis dolor magni ea laboriosam deserunt alias repudiandae
+          aliquid error!
+        </p>
+        <a
+          href="#"
+          className="inline-block bg-fuchsia-800 px-6 py-2 rounded-full hover:bg-fuchsia-600"
+        >
+          Click Me
+        </a>
+      </section>
+    </div>
+  );
+}
