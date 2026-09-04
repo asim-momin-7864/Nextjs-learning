74eb7ddd9e5d52f106b176d8a4a532c5af7355bc
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-09-04 20:12:53 +0530

    clean up and file arrangemenet

---
+++ pizza-ordering-app/src/app/(home)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      Hello pizza Wizza
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
+        accusantium praesentium officiis iusto, amet aspernatur nemo porro fugit
+        sit quis nobis dolorem eaque nesciunt repudiandae illum reprehenderit?
+        Nesciunt, inventore architecto?
+      </p>
+    </div>
+  );
+}
