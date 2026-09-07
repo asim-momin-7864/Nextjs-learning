97555cd8b2ede94a15496e0a729c928c14d8fefa
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-09-07 22:56:53 +0530

    implemented reference form project

--- form-learning-app/src/app/layout.tsx
+++ form-learning-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Noto_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Toaster } from "sonner";
 
 const geistHeading = Geist({subsets:['latin'],variable:'--font-heading'});
 
@@ -28,7 +29,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", notoSans.variable, geistHeading.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Toaster richColors position="top-right" />
+      </body>
     </html>
   );
 }
