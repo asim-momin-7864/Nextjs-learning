dcd6ca07c81869cc77c1538513575f624c83c1ad
“asim-momin-7864” <asimmomin.dev@gmail.com>
2026-09-14 01:36:14 +0530

    feat: add dashboard layout with CreatePostForm and PostList components

--- tanstack-learning-app/src/app/page.tsx
+++ tanstack-learning-app/src/app/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
+import { PostList } from "@/components/custome/post-list";
+import { CreatePostForm } from "@/components/custome/create-post-form";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
-    </div>
+    <main className="mx-auto max-w-5xl px-4 py-10 space-y-10">
+      {/* header */}
+      <header className="space-y-1">
+        <h1 className="text-3xl font-bold tracking-tight">
+          Micro-Blog Dashboard
+        </h1>
+      </header>
+
+      {/* main content */}
+      <div className="grid gap-8 lg:grid-cols-2">
+        <aside>
+          <CreatePostForm />
+        </aside>
+
+        {/* right */}
+        <section>
+          <h2 className="mb-4 text-xl font-semibold">Recent Posts</h2>
+          <PostList />
+        </section>
+      </div>
+    </main>
   );
 }
